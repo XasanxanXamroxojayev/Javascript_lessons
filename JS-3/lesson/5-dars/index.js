6c740399e86c237b76c933e27c94ad6446a9427c
XasanxanXamroxojayev <xasanxonxamroxojayev@gmail.com>
2026-07-23 15:03:59 +0500

    JS malumoq qo'shyapman

---
+++ JS-3/lesson/5-dars/index.js
@@ -0,0 +1,23 @@
+// let arr = [1, 2, 2, 3, 1, 7, 5, 9, 9]
+// let num = []
+// let newArr = arr.filter((n) => {
+//     if (!num.includes(n)) {
+//         return num.push(n)
+//     }
+// })
+// console.log(newArr);
+
+// ============================================================================
+
+
+// let str = "Men Oxford da o'qimoqdaman."
+// let newStr = str.split("")
+// let setArr = new Set(newStr)
+// console.log(Array.from(setArr).join(""));
+
+
+
+
+
+
+
