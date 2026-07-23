e8a6da2dc112b4d1953c630c55eadf1f94af59b9
XasanxanXamroxojayev <xasanxonxamroxojayev@gmail.com>
2026-07-23 22:34:12 +0500

    yangi JS malumot

---
+++ JS-3/lesson/6-dars/index.js
@@ -0,0 +1,24 @@
+// WeakMap, WeakSet
+
+
+// let map = new WeakMap()
+// let key = {}
+// map.set(key, "Eshmat")
+// console.log(map.get(key));
+// console.log(map.has(key));
+// map.delete(key)
+// console.log(map);
+// console.log(map.get(key));
+// map = null
+
+
+
+// let set = new WeakSet()
+// let value = {id: 1}
+// set.add(value)
+
+// console.log(set.has(value));
+// set.delete(value)
+// console.log(set.has(value));
+// console.log(set);
+
